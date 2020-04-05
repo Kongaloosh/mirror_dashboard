bea9d76fdc4735fd59506eb64c64255260248d32
kongaloosh <hi@kongaloosh.com>
2020-04-04 19:05:44 -0600

    Added auto-loading of datetime and weather updates.

--- static/js/weather_widget.js
+++ static/js/weather_widget.js
@@ -15,29 +15,29 @@ monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"]
 
 dayNames = ['Mon', "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"]
-var request = new XMLHttpRequest()
-var today = new Date();
 
+var request = new XMLHttpRequest()
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', '/weather', true)
 
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-
-  if (request.status >= 200 && request.status < 400) {
-    document.getElementById("weather-icon").className = 'fa ' + icons[data['weather'][0]['description']]
-    document.getElementById("weather-temp").innerText = data['main']['feels_like'].toString() + '\u2103'
-
+window.setInterval(function(){
+    var today = new Date();
     document.getElementById("date").innerText = dayNames[today.getDay()-1]+' '+monthNames[today.getMonth()]+' '+today.getDate();
     document.getElementById("time").innerText = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+}, 1000);
 
+
+window.setInterval(function(){
+    var data = JSON.parse(request.response)
+    // Send request
+    if (request.status >= 200 && request.status < 400) {
+        document.getElementById("weather-icon").className = 'fa ' + icons[data['weather'][0]['description']]
+        document.getElementById("weather-temp").innerText = data['main']['feels_like'].toString() + '\u2103'
     console.log(data)
   } else {
     console.log('error')
   }
-}
 
+}, 10000);
 
-// Send request
 request.send()
