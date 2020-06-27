63203e7388bec9a313b1142fb81165273e63d2f2
kongaloosh <hi@alexkearney.com>
2020-06-27 17:22:10 -0600

    changed the way data is requested from the garden.

--- static/js/garden.js
+++ static/js/garden.js
@@ -6,10 +6,10 @@ function garden_HUD(){
             console.log(request)
             if(request.status >= 200 && request.status < 400){
                 request.json().then(function(data) {
-                    document.getElementById("garden_temperature").innerText = "Temperature: " + data['temperature'][0].toString() + '\u2103';
-                    document.getElementById("garden_moisture").innerText = "Soil Mositure: " + ((1 - data['moistness'][0])*100).toPrecision(2).toString() + '\u0025';
-                    document.getElementById("garden_humidity").innerText = "Humidity: " + data['humidity'][0].toString() + '\u0025';
-                    document.getElementById("garden_pic").src = "http://192.168.1.12/" + data['image'][0]
+                    document.getElementById("garden_temperature").innerText = "Temperature: " + data['temperature'].shift().toString() + '\u2103';
+                    document.getElementById("garden_moisture").innerText = "Soil Mositure: " + ((1 - data['moistness'].shift())*100).toPrecision(2).toString() + '\u0025';
+                    document.getElementById("garden_humidity").innerText = "Humidity: " + data['humidity'].shift().toString() + '\u0025';
+                    document.getElementById("garden_pic").src = "http://192.168.1.12/" + data['image'].shift()
                 });
             }else {
                 console.log('error')
@@ -20,4 +20,4 @@ function garden_HUD(){
 
 garden_HUD()
 
-window.setInterval(garden_HUD, 1000000);
+window.setInterval(garden_HUD, 60000);
